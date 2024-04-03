ee3f90cc3d142839dc7e9a8574d20c83a979496e
Mark-obov <obov@sl-platform.com>
2024-04-03 23:06:51 +0900

    feat: add helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>타이틀을 바꾸고 meta tag를 넣음</title>
+        <meta property="og:title" content="og title" />
+        <meta
+          property="og:description"
+          content="og description og description og description og description"
+        />
+        <meta
+          property="og:image"
+          content="https://source.unsplash.com/random/300×300"
+        />
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
